6e6fa4cb6fe5f52e2e415ee4a4822f448363d4d0
adeeburrahman11 <adeeburrahman9021@gmail.com>
2024-06-25 12:14:59 +0530

    home page half

---
+++ lib/start_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          const Text(
+            'Learn flutter fun way',
+          )
+        ],
+      ),
+    );
+  }
+}
