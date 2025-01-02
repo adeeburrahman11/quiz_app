785a2f5e91f5940e137dca7d3645863cce668fba
adeeburrahman11 <adeeburrahman9021@gmail.com>
2025-01-02 22:33:19 +0530

    resultscreen ui updated

---
+++ lib/questions_summary/check_questions.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/data/colors.dart';
+
+class CheckQuestions extends StatelessWidget {
+  const CheckQuestions(
+      {super.key, required this.questionIndex, required this.isCorrectAnswer});
+
+  final int questionIndex;
+  final bool isCorrectAnswer;
+
+  @override
+  Widget build(BuildContext context) {
+    final questionNumber = questionIndex + 1;
+    return Container(
+      width: 30,
+      height: 30,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: isCorrectAnswer ? correct : wrong,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Text(
+        questionNumber.toString(),
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          color: primary,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/questions_summary/summary_item.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:quiz_app/data/colors.dart';
+import 'package:quiz_app/questions_summary/check_questions.dart';
+
+class SummaryItem extends StatelessWidget {
+  const SummaryItem(this.itemData, {super.key});
+
+  final Map<String, dynamic> itemData;
+
+  @override
+  Widget build(BuildContext context) {
+    final isCorrectAnswer =
+        itemData['user_answer'] == itemData['correct_answer'];
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CheckQuestions(
+          questionIndex: itemData['question_index'],
+          isCorrectAnswer: isCorrectAnswer,
+        ),
+        const SizedBox(
+          width: 20,
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                itemData['question'] as String,
+                style: GoogleFonts.lato(
+                  color: primary,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Text(
+                itemData['user_answer'] as String,
+                style: GoogleFonts.lato(
+                  color: isCorrectAnswer ? correct : wrong,
+                  fontSize: 18,
+                ),
+              ),
+              Text(
+                itemData['correct_answer'] as String,
+                style: GoogleFonts.lato(
+                  color: correct,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
